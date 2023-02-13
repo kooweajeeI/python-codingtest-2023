e3a5ed6b101d9c0fb8328130f26fa2b8ae0d84eb
kooweajeel-it <leejw2873@naver.com>
2023-02-13 17:29:26 +0900

    2월 13일 완.

---
+++ Day01/ct04_section_sum.py
@@ -0,0 +1,16 @@
+# 입력속도 개선. 단, 주피터노트북에서는 실행불가
+import sys
+input = sys.stdin.readline      # 이 부분이 없으면 백준 시간초과
+
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+sums = [0]      # 배열 0번째 인덱스
+temp = 0
+
+for i in numbers:
+    temp += i
+    sums.append(temp)
+
+for i in range(M):
+    x, y = map(int, input().split())
+    print(sums[y] - sums[x-1])
