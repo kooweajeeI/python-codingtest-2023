22f328c8e825ce995f14f52cf9ff2991ab457185
kooweajeel-it <leejw2873@naver.com>
2023-02-17 17:24:58 +0900

    2월 17일 완.

---
+++ Day05/ds24_windowsystem.py
@@ -0,0 +1,25 @@
+# C:\Windows\System32 파일 출력
+
+import os
+def makeFileList(folder):
+    fnameAry = []
+    folder = 'C:/Windows/System32'
+    for dirName, surDirList, fnames in os.walk(folder):
+        for fname in fnames:
+            fnameAry.append(fname)
+    return fnameAry
+
+def insertionSort(ary):
+    n = len(ary)
+    for end in range(1, n):     # 앞의 값 하나 빼고 끝까지 반복
+        for cur in range(end, 0, -1):   # 반대로 돌면서
+            if ary[cur-1] < ary[cur]:   # 내림차순
+                # tmp = ary[cur-1]
+                # ary[cur-1] = ary[cur]
+                # ary[cur] = tmp
+                ary[cur-1], ary[cur] = ary[cur], ary[cur-1] # 파이썬 swap
+    return ary
+
+fileAry = makeFileList('C:/Program Files/Common Files')
+fileAry = insertionSort(fileAry)
+print('파일명 역순 -->', fileAry)
